c727805ff2285fd726d277359e09721c8696c66f
JHIH-LEI <vicky88888@gmail.com>
2021-07-21 22:53:24 +0800

    feat: add moment & format date

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const express = require('express')
 const app = express()
 
 const exphbs = require('express-handlebars')
+const moment = require('moment')
 require('./config/mongoose')
+const Record = require('./models/record')
 const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Record.find()
+    .lean()
+    .then(records => {
+      records.forEach(rc => {
+        rc.date = moment(rc.date).format('MMMM d dddd, YYYY')
+      })
+      res.render('index', { records })
+    })
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
