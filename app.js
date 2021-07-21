4011a8c68b3f3f8c95adb6e398db89af51f50c32
JHIH-LEI <vicky88888@gmail.com>
2021-07-21 23:07:52 +0800

    feat: get categoryList from db and send to home page

--- app.js
+++ app.js
@@ -5,19 +5,25 @@ const exphbs = require('express-handlebars')
 const moment = require('moment')
 require('./config/mongoose')
 const Record = require('./models/record')
+const Category = require('./models/category')
 const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
+  let categoryList = []
+  Category.find()
+    .lean()
+    .then(category => categoryList = category)
   Record.find()
     .lean()
     .then(records => {
       records.forEach(rc => {
         rc.date = moment(rc.date).format('MMMM d dddd, YYYY')
       })
-      res.render('index', { records })
+      res.render('index', { records, categoryList })
+      console.log(category)
     })
     .catch(error => console.log(error))
 })
