378f949b0eff261bae261607dcb627ac1b02c5d5
JHIH-LEI <vicky88888@gmail.com>
2021-07-22 02:51:52 +0800

    feat: add dateFormat function
    轉換成YYYY-MM-DD格式，才能在編輯資料時帶入date預設值

---
+++ tools/dateFormat.js
@@ -0,0 +1,9 @@
+function dateFormat(Date) {
+  const year = Date.getFullYear()
+  const month = (Date.getMonth() + 1) < 10 ? '0' + Date.getMonth().toString() : Date.getMonth()
+  const date = Date.getDate() < 10 ? '0' + Date.getDate().toString() : Date.getDate()
+  //將資料原日期傳至input date value
+  return `${year}-${month}-${date}`
+}
+
+module.exports = dateFormat
