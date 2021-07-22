f80ad51b8bb33cd1a727e75e2cc57acf0c74249f
JHIH-LEI <vicky88888@gmail.com>
2021-07-23 00:48:34 +0800

    feat: add new category function & fix edit & new page bug
    因爲非同步問題，導致編輯頁的類別選單無法拿到類別資料，解決方案為先抓類別資料，抓完之後才執行record的查找。
    render 新增資料頁面時，忘記給他類別資料。錯誤跟成功訊息則是在使用者新增類別後，重新導向至該頁面時會給予使用者的提示。

--- app.js
+++ app.js
@@ -5,15 +5,30 @@ const exphbs = require('express-handlebars')
 const moment = require('moment')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
+// 使用flash
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const flash = require('connect-flash')
+
 require('./config/mongoose')
 const Record = require('./models/record')
 const Category = require('./models/category')
 const { getIcon } = require('./public/javascripts/helper')
 const dateFormat = require('./tools/dateFormat')
+const iconsClass = require('./categoryIcon')
 const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+// 使用flash
+app.use(cookieParser());
+app.use(session({
+  secret: 'secret123',
+  saveUninitialized: true,
+  resave: true
+}));
+app.use(flash());
 
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -46,7 +61,7 @@ app.get('/record/new', (req, res) => {
     .lean()
     .then(category => {
       categoryList = category
-      res.render('new', { categoryList })
+      res.render('new', { categoryList, iconsClass, error: req.flash('error'), success: req.flash('success') })
     })
     .catch(error => console.log(error))
 })
@@ -60,11 +75,19 @@ app.post('/record/new', (req, res) => {
 
 app.get('/record/:id/edit', (req, res) => {
   const id = req.params.id
-  Record.findById(id)
+  let categoryList = []
+  Category.find()
     .lean()
-    .then(record => {
-      const time = dateFormat(record.date)
-      res.render('edit', { record, time })
+    .then(category => {
+      categoryList = category
+    })
+    .then(() => {
+      Record.findById(id)
+        .lean()
+        .then(record => {
+          const time = dateFormat(record.date)
+          res.render('edit', { record, time, categoryList })
+        })
     })
     .catch(error => console.log(error))
 })
@@ -90,6 +113,37 @@ app.put('/record/:id', (req, res) => {
     })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
+})
+
+app.post('/category', (req, res) => {
+  const newName = req.body.newCategory
+  // icon的網址，取得使用者勾選的項目，其value(也就是類別圖示)
+  const icon = req.body.icon
+  // 上一頁網址
+  const url = req.headers.referer
+  const pathname = new URL(url).pathname
+  // 檢查類別是否已存在，如果存在回傳true，用then是因為返回的是未實現的promise
+  Category.exists({ name: newName })
+    .then(boolean => {
+      //如果類別已存在
+      if (boolean) {
+        // 如果上一頁是新增頁面
+        if (pathname.indexOf('new') > 0) {
+          // 返回新增頁面，並傳送錯誤提示
+          req.flash('error', '類別已經存在')
+          res.redirect(pathname)
+          // 如果上一頁是修改頁面，返回該頁並傳送錯誤提示
+        } else if (pathname.indexOf('edit')) res.render('edit', { iconsClass, error: '類別已經存在' })
+        // 如果類別尚未存在
+      } else {
+        // 儲存新類別到資料庫，返回上一頁
+        Category.create({ name: newName, icon })
+          .then(() => {
+            req.flash('success', `${newName}   已經新增到類別當中！`)
+            res.redirect(pathname)
+          })
+      }
+    })
 })
 
 app.listen(port, () => {
