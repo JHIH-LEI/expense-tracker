54364475fd856fe8b9d722af584ba1f863a48adf
JHIH-LEI <vicky88888@gmail.com>
2021-07-23 00:41:51 +0800

    feat: add new category icons list

---
+++ categoryIcon.js
@@ -0,0 +1,3 @@
+const iconsClass = ['fas fa-music', 'fas fa-baseball-ball', 'fas fa-school', 'fas fa-book', 'fas fa-gamepad', 'fas fa-palette', 'fas fa-cart-arrow-down', 'fas fa-laptop', 'fas fa-tshirt', 'fas fa-dog', 'fas fa-cat', 'fas fa-tools', 'fas fa-people-arrows']
+
+module.exports = iconsClass
