309b3522afce91247e38b34bf4164b78b1a96e27
JHIH-LEI <vicky88888@gmail.com>
2021-07-23 02:12:55 +0800

    fix: fix edit page data date reduce one month error

--- tools/dateFormat.js
+++ tools/dateFormat.js
@@ -1,6 +1,6 @@
 function dateFormat(Date) {
   const year = Date.getFullYear()
-  const month = (Date.getMonth() + 1) < 10 ? '0' + Date.getMonth().toString() : Date.getMonth()
+  const month = (Date.getMonth() + 1) < 10 ? '0' + (Date.getMonth() + 1).toString() : Date.getMonth()
   const date = Date.getDate() < 10 ? '0' + Date.getDate().toString() : Date.getDate()
   //將資料原日期傳至input date value
   return `${year}-${month}-${date}`
