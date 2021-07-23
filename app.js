bb171d61e3710542426dfd5ac6cb15763117bd90
JHIH-LEI <vicky88888@gmail.com>
2021-07-24 01:47:50 +0800

    feat: add income & expense record function on edit page
    能根據紀錄是支出還是收入來改變表單設定、新增類別設定、表單提示字、要儲存的金額是正還是負，另外在get edit page時，也會先將金額一律轉為正數，讓使用者不需要考慮正負數的問題，針對金額相關的更改正負數設定，用money變數實現（因為如果直接操作record的amount，會出現不可更改const 變數的錯誤，改成在新增或修改資料「時」來操作，而非「之前」，可見money的賦予會出現在資料操作的當下，而非之前），並assign 給record資料中的amount

--- app.js
+++ app.js
@@ -99,8 +99,9 @@ app.post('/record/new', (req, res) => {
     .catch(error => console.log(error))
 })
 
-app.get('/record/:id/edit', (req, res) => {
+app.get('/record/:id/edit/:type', (req, res) => {
   const id = req.params.id
+  const type = req.params.type
   let categoryList = []
   Category.find()
     .lean()
@@ -111,9 +112,11 @@ app.get('/record/:id/edit', (req, res) => {
       Record.findById(id)
         .lean()
         .then(record => {
-          console.log(record.date)
+          // 金額都是正的，所以在render編輯頁前要先把支出的負數拿掉
+          let money = 0
+          money = record.amount < 0 ? record.amount * -1 : record.amount
           const time = dateFormat(record.date)
-          res.render('edit', { record, time, categoryList, iconsClass, error: req.flash('error'), success: req.flash('success') })
+          res.render('edit', { type, record, money, time, categoryList, iconsClass, error: req.flash('error'), success: req.flash('success') })
         })
     })
     .catch(error => console.log(error))
@@ -130,15 +133,20 @@ app.delete('/record/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
-app.put('/record/:id', (req, res) => {
+app.put('/record/:type/:id', (req, res) => {
+  // 修改資料前，將支出金額轉負，收入轉正
+  const type = req.params.type
   const { name, date, category, amount } = req.body
+  let money = 0
+  // 根據類別類型來調整金額正負(type為收入或支出)
+  money = type === '收入' ? amount : 0 - amount
   const id = req.params.id
   Record.findById(id)
     .then(record => {
       record.name = name
       record.date = date
       record.category = category
-      record.amount = amount
+      record.amount = money
       record.save()
     })
     .then(() => res.redirect('/'))
