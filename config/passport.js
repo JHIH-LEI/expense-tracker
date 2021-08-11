5f9b814b2c06d87e183a69633a3ef4c8265ef37c
JHIH-LEI <vicky88888@gmail.com>
2021-08-11 16:48:10 +0800

    feat: add facebook login via passport-facebook

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
-const LocalStrategy = require('passport-local')
+const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
 const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 
@@ -24,6 +25,32 @@ module.exports = (app) => {
       })
       .catch(err => done(err, null))
   }));
+
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: process.env.FACEBOOK_APP_CALLBACK,
+    profileFields: ['email', 'displayName']
+  },
+    (accessToken, refreshToken, profile, done) => {
+      const { email, name } = profile._json
+      User.findOne({ email })
+        .then(user => {
+          if (user) {
+            return done(null, user)
+          }
+          const randomPassword = Math.random().toString(36).slice(-8)
+          return bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(randomPassword, salt))
+            .then(hash => {
+              User.create({ email, name, password: hash })
+            })
+            .then(() => done(null, user))
+        })
+        .catch(err => done(err, null))
+    }
+  ));
   // 序列化/反序列化
   passport.serializeUser((user, done) => {
     done(null, user.id);
--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,14 @@ const record = require('./modules/record')
 const category = require('./modules/category')
 const chart = require('./modules/chart')
 const user = require('./modules/user')
+const auth = require('./modules/auth')
 const { authenticator } = require('../middleware/auth')
 
 router.use('/chart', authenticator, chart)
 router.use('/record', authenticator, record)
 router.use('/category', authenticator, category)
 router.use('/user', user)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
